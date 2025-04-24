40c73546e70c618536680590a7237c8c2670f9d0
timetetng <149919795+timetetng@users.noreply.github.com>
2025-04-25 02:44:27 +0800

    初始化 config.py

--- config.py
+++ config.py
@@ -1,23 +1,23 @@
 # config.py
-QQBOT_API_URL = 'http://8.138.187.179:3000'
-GROUP_ID = 932411215  # 群号
+QQBOT_API_URL = 'http://' #Napcat HTTP服务器URL
+GROUP_ID = xxxx  # 群号
 
 # 定义一个 RSS 源列表
 RSS_FEEDS = [
     {
-        'TG_GROUP_NAME': 'stepleaker',
-        'RSS_URL': 'https://rsshub.app/telegram/channel/stepleaker'
+        'TG_GROUP_NAME': 'xxx',
+        'RSS_URL': 'https://rsshub.app/telegram/channel/xxx'
     },
     {
-        'TG_GROUP_NAME': 'WuWaZeta',
-        'RSS_URL': 'https://rsshub.app/telegram/channel/WuWaZeta'  
+        'TG_GROUP_NAME': 'yyy',
+        'RSS_URL': 'https://rsshub.app/telegram/channel/yyy'  
     },
         {
-        'TG_GROUP_NAME': 'Seele',
-        'RSS_URL': 'https://rsshub.app/telegram/channel/Seele_WW_Leak'  
+        'TG_GROUP_NAME': 'zzz',
+        'RSS_URL': 'https://rsshub.app/telegram/channel/zzz'  
     },
 
     # 可以继续添加更多的频道
 ]
 # 抓取周期，单位为分钟
-FETCH_INTERVAL = 1
+FETCH_INTERVAL = 10
