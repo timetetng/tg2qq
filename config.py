b3a46549eaa7682dfa0660a77482febeefb66847
timetetng <149919795+timetetng@users.noreply.github.com>
2025-04-26 00:57:34 +0800

    添加可选配置项：支持LLM模型格式化消息

--- config.py
+++ config.py
@@ -31,3 +31,25 @@ RSS_FEEDS = [
 ]
 # 抓取周期，单位为分钟
 FETCH_INTERVAL = 10
+
+# 是否使用LLM格式化 (默认关闭)
+is_LLM = False
+# LLM 请求间隔，单位为秒
+req_interval = 3
+# 以gemini为例，若使用其他LLM服务商，修改下面API_KEY配置以及llm_utils.py调用代码即可
+# Gemini API Key
+API_KEY = "AIzaSyB-oxgQS9wL93OO0i4ULAq-jmq5Hsvnajs"
+# 模型
+MODEL_NAME = 'models/gemini-2.0-flash'
+# 系统提示词，可根据需求调整，比如添加双语翻译
+SYSTEM_PROMPT = """
+你是一个文本格式化工具，你的任务是修复给定文本中的格式问题，例如：
+- 修复不正确的缩进
+- 修复不正确的换行
+- 移除多余的空格
+- 确保文本清晰易读
+下面是一些要求：
+如遇到形如："Image_563099568650661.png
+Image_563183155179588.png 3.9 MB"的文本，说明该图片无法获取，替换为”【无法获取图片 xx MB（如果有大小）】“即可,对于某消息含有“Video is too big”说明此处有视频过大无法爬去，同样替换为“【视频过大无法获取】”
+请保持文本的原始结构和内容不变，只修复格式问题，如果用户本次没有输入或没有待格式化/翻译内容，则返回原文即可。
+"""
